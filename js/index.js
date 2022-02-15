ec5e9aa6116065a61f2519d642159e56109e0eeb
valery makarov <valerythf@outlook.com>
2022-02-15 21:08:03 +0300

    adds animations
    v1.0

--- js/index.js
+++ js/index.js
@@ -103,3 +103,97 @@ document.querySelector('.feedback__slider-prev').addEventListener('click', funct
 	}	
 	feedbackSliderLine.style.left = -feedbackSliderOffset + 'px';
 })
+
+
+// ANIMATION
+
+const preAnimated = document.querySelectorAll('.pre-animated');
+
+let animationCounter = true;
+
+window.addEventListener('scroll', scrollAnimation);
+function scrollAnimation() {
+	for (let i = 0; i < preAnimated.length; i++) {
+		const itemToAnimate = preAnimated[i];
+		const itemHight = itemToAnimate.offsetHeight;
+		const itemOffset = offset(itemToAnimate).top;
+		const itemStart = 10;
+
+		let itemToAnimatePlace = window.innerHeight - itemHight / itemStart;
+		if (itemHight > window.innerHeight) {
+			itemToAnimatePlace = window.innerHeight - window.innerHeight / itemStart;
+		}
+
+		if ((pageYOffset > itemOffset - itemToAnimatePlace) && (pageYOffset < (itemOffset + itemToAnimatePlace))) {
+			itemToAnimate.classList.add('animated');
+			if (itemToAnimate.classList.contains('uncounted-percent')) {
+				countStatsPercent();
+			}
+			if (itemToAnimate.classList.contains('uncounted-stat')) {
+				countStats();
+			}
+		}
+	}
+
+	function offset(element) {
+		const rect = element.getBoundingClientRect();
+		const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+		const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		return { top: rect.top + scrollTop, left: rect.left + scrollLeft}
+	}
+}
+
+function countPercent(num, place) {
+	const time = 2000;
+	const step = 5;
+
+	let start = 0;
+
+	let object = document.querySelector(place);
+	let iterations = Math.round(time / (num / step));
+
+	let interval = setInterval(() => {
+		start += step;
+		if (start == num) {
+			clearInterval(interval);
+		}
+		object.innerHTML = start + '%';
+	}, iterations);
+}
+
+function countNumber(num, place) {
+	const time = 2000;
+	const step = 5;
+
+	let start = 0;
+
+	let object = document.querySelector(place);
+	let iterations = Math.round(time / (num / step));
+
+	let interval = setInterval(() => {
+		start += step;
+		if (start == num) {
+			clearInterval(interval);
+		}
+		object.innerHTML = start;
+	}, iterations);
+}
+
+function countStatsPercent() {
+	countPercent(80, '.capabilities_softwear .capabilities__percent');
+	countPercent(90, '.capabilities_graphic .capabilities__percent');
+	countPercent(85, '.capabilities_game .capabilities__percent');
+	countPercent(100, '.capabilities_web .capabilities__percent');
+	document.querySelector('.capabilities__list').classList.remove('uncounted-percent')
+}
+
+function countStats() {
+	countNumber(120, '.capabilities_experts .capabilities__number');
+	countNumber(500, '.capabilities_strategies .capabilities__number');
+	countNumber(345, '.capabilities_results .capabilities__number');
+	countNumber(120, '.capabilities_rankings .capabilities__number');
+	document.querySelector('.capabilities__stats').classList.remove('uncounted-stat')
+}
+
+
+scrollAnimation();
